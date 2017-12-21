5b82725359f53272e4618c413f412489ae79e5aa
Fred Hirsch <webmosher@gmail.com>
2017-12-21 14:57:31 -0700

    Part 6 - Final

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,14 +4,20 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
